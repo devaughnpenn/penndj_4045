641c5b189dd25f9dadec6fd50e1136e9216e185e
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-17 14:55:39 -0400

    9/17/19 Commit

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -1,5 +1,21 @@
 package com.plantplaces;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class PlantPlacesController {
 
+	@RequestMapping("/start")
+	public String start() {
+		return "start";
+	}
+	/**
+	 *  Handle the / endpoint
+	 * @return
+	 */
+	@RequestMapping("/")
+	public String index() {
+		return "start";
+	}
 }
